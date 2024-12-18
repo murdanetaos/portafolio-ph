5700824117b1df2b3a3357943607ebba1f0c941e
murdanetaos <miguelurd10@gmail.com>
2024-12-18 15:33:24 -0500

    making development branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './component/Routes/Home';
 import About from './component/Routes/About';
 import Gallery from './component/Routes/Gallery';
